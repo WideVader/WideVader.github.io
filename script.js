7281b650ee54bf17cb0ff0a3e53a654ac6793429
WideVader <74324332+WideVader@users.noreply.github.com>
2022-03-23 14:41:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var sidePanel = document.getElementById("sidePanel");
-console.log(sidePanel);
- window.addEventListener("scroll",async ()=>{
-    await Promise.resolve(10000);
+ window.addEventListener("scroll",()=>{
     if(window.scrollY>window.innerHeight/2){
         sidePanel.className = "sidePanelC";
     }
